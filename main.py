175de487ec42dd23bc6554df15348bddc4149849
Lior Daniel <lior7daniel@gmail.com>
2024-05-06 18:20:28 +0300

    AWS Integration

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from src.aws.handlers.iam_users import IAMHandler
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main():
+    # Instantiate IAMHandler
+    iam_handler = IAMHandler()
+    iam_users = iam_handler.fetch_data()
+
+    # Process IAM users data or perform other operations
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
